6fd14d1ab51948a671f0b87bfdedac01686fe30e
Mike Artz <fartzy@hotmail.com>
2020-10-02 08:34:15 -0700

    Import mp module

--- tests/test_make_requests_fast.py
+++ tests/test_make_requests_fast.py
@@ -1,5 +1,10 @@
 from make_requests_fast import __version__
+import multiprocessing as mp
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def test_process_spawn():
+    pass

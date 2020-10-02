ab0818f23019387e44d8f3a22872830b69a63546
Mike Artz <fartzy@hotmail.com>
2020-10-02 08:31:38 -0700

    Adding condition to handle MultiprocessThreadPoolfrom cli

--- make_requests_fast/cli.py
+++ make_requests_fast/cli.py
@@ -6,6 +6,9 @@ from make_requests_fast.utils.ListReader import ListReader
 from make_requests_fast.requestors.BufferedChunkedThreadPoolRequestor import (
     BufferedChunkedThreadPoolRequestor,
 )
+from make_requests_fast.requestors.MultiprocessThreadPoolRequestor import (
+    MultiprocessThreadPoolRequestor,
+)
 from make_requests_fast.requestors.ChunkedProcessPoolRequestor import ChunkedProcessPoolRequestor
 from make_requests_fast.requestors.ChunkedThreadPoolRequestor import ChunkedThreadPoolRequestor
 from make_requests_fast.requestors.SequentialRequestor import SequentialRequestor
@@ -55,6 +58,11 @@ def run(
         cppr = ChunkedProcessPoolRequestor(file)
         cppr.execute()
         log_file = cppr.log_path
+
+    elif requestor == "MultiprocessThreadPool":
+        mtpr = MultiprocessThreadPoolRequestor(file)
+        mtpr.execute()
+        log_file = mtpr.log_path
 
     typer.echo(f"\nExecuted {requestor} with {file}")
     typer.echo(f"Log - {log_file}")
